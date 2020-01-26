94cfc15ba77640446d3920c54d1f839588b9269d
phoborsh <phoeniiiixj@gmail.com>
2020-01-26 22:36:06 +0100

    make it better

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ const addon = new addonBuilder({
 })
 
 var addic7edURL = "www.addic7ed.com"
-// var local = "http://addic7ed-stremio-addon-heroku.herokuapp.com"
-var local = "http://stremio-addic7ed.pho.ovh"
+var local = "http://addic7ed-stremio-addon-heroku.herokuapp.com"
 var Languages = ["eng","fre","esp","ita","swe","srp","slv","slk","rom","por","nor","mol","lit","lim","nld","lav","heb","fin","gre","ger","deu","cze","ara"]
 const route2referer = {}
 
