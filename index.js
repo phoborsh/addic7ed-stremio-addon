74593665fc39480d85c530ea7d07c634644467c8
phoborsh <phoeniiiixj@gmail.com>
2020-01-27 01:03:57 +0100

    heroku push

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ async function GetShowSubsList(Name,SeasonId,EpisodeId){
 async function GetMovieSubsList(Name){
 	let subtitlesList = await searchMovie(Name)
 	console.log(subtitlesList)
+	console.log("FOUND SUBS!!!!")
 	return subtitlesList
 }
 
